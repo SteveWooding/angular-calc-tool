cfab613884fa3360a28c67ff0c478c9fa12dc5cf
Steven Wooding <steve@stevenwooding.com>
2016-04-04 14:50:49 +0100

    feat: Set up the select options

--- assets/calculator.js
+++ assets/calculator.js
@@ -6,13 +6,10 @@
 
   app.controller('CalculatorController', ['$scope', function($scope) {
 
+    $scope.lumenOptions = [375, 600, 900, 1125, 1600];
+    $scope.currentLumens = 600;
+
   }]);
-
-
-
-
-
-
 
 })();
 
