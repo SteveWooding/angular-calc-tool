b0115ef728ff6dfb1da52c4642ed070d5a3e635d
Steven Wooding <steve@stevenwooding.com>
2016-06-09 12:35:51 +0100

    feat: Add bulb lifetime and total 10 year cost
    Not too happy with the implmentation, extending the code style
    of the instructor. May refactor it later.

--- assets/calculator.js
+++ assets/calculator.js
@@ -19,6 +19,18 @@
     var cflConversion = 0.0181;
     var ledConversion = 0.0100;
 
+    // Store the lifetimes of each bulb
+    $scope.incLife = 1000;
+    $scope.halLife = 2000;
+    $scope.cflLife = 10000;
+    $scope.ledLife = 35000;
+
+    // Store the replacement cost of each bulb
+    var incBulbCost = 0.63;
+    var halBulbCost = 1.40;
+    var cflBulbCost = 2.00;
+    var ledBulbCost = 3.20;
+
     /**
      * Calculate the wattage and cost of each bulb type.
      */
@@ -45,6 +57,16 @@
       $scope.halCost = ((($scope.halWattage * totalHours) / 1000) * cost).toFixed(2);
       $scope.cflCost = ((($scope.cflWattage * totalHours) / 1000) * cost).toFixed(2);
       $scope.ledCost = ((($scope.ledWattage * totalHours) / 1000) * cost).toFixed(2);
+
+      // Calculate the 10 year cost, with bulb replacement.
+      $scope.inc10yCost = (10 * $scope.incCost + incBulbCost *
+        Math.ceil(totalHours * 10 / $scope.incLife)).toFixed(2);
+      $scope.hal10yCost = (10 * $scope.halCost + halBulbCost *
+        Math.ceil(totalHours * 10 / $scope.halLife)).toFixed(2);
+      $scope.cfl10yCost = (10 * $scope.cflCost + cflBulbCost *
+        Math.ceil(totalHours * 10 / $scope.cflLife)).toFixed(2);
+      $scope.led10yCost = (10 * $scope.ledCost + ledBulbCost *
+        Math.ceil(totalHours * 10 / $scope.ledLife)).toFixed(2);
     }
 
     // Run the calculation the first time the page is loaded.
