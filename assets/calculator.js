b5243bdd84d85706e1422c8686121762938f369a
Steven Wooding <steve@stevenwooding.com>
2016-06-29 16:27:07 +0100

    refactor: Change to Controller As sytax rather than $scope
    It is generally recommended to use the Controller As
    syntax rather than directly accessing the $scope
    variable. This is important for readability if controllers are
    nested.

--- assets/calculator.js
+++ assets/calculator.js
@@ -4,13 +4,16 @@
 
   var app = angular.module('myCalculator', []);
 
-  app.controller('CalculatorController', ['$scope', function($scope) {
+  app.controller('CalculatorController', function() {
+
+    // Capture the controller context in a view model variable
+    var vm = this;
 
     // Set default values and options
-    $scope.lumenOptions = [210, 420, 630, 1050, 1600];
-    $scope.currentLumens = 630;
-    $scope.currentCost = 12;
-    $scope.currentHours = 3;
+    vm.lumenOptions = [210, 420, 630, 1050, 1600];
+    vm.currentLumens = 630;
+    vm.currentCost = 12;
+    vm.currentHours = 3;
     var totalDays = 365;
 
     // Define the approximate conversion parameter for each bulb type.
@@ -20,10 +23,10 @@
     var ledConversion = 0.0100;
 
     // Store the lifetimes of each bulb
-    $scope.incLife = 1000;
-    $scope.halLife = 2000;
-    $scope.cflLife = 10000;
-    $scope.ledLife = 35000;
+    vm.incLife = 1000;
+    vm.halLife = 2000;
+    vm.cflLife = 10000;
+    vm.ledLife = 35000;
 
     // Store the replacement cost of each bulb
     var incBulbCost = 0.63;
@@ -34,44 +37,44 @@
     /**
      * Calculate the wattage and cost of each bulb type.
      */
-    $scope.calculate = function() {
+    vm.calculate = function() {
       // Calculate the wattage of each bulb type.
-      $scope.incWattage = ($scope.currentLumens * incConversion).toFixed(1);
-      $scope.halWattage = ($scope.currentLumens * halConversion).toFixed(1);
-      $scope.cflWattage = ($scope.currentLumens * cflConversion).toFixed(1);
-      $scope.ledWattage = ($scope.currentLumens * ledConversion).toFixed(1);
+      vm.incWattage = (vm.currentLumens * incConversion).toFixed(1);
+      vm.halWattage = (vm.currentLumens * halConversion).toFixed(1);
+      vm.cflWattage = (vm.currentLumens * cflConversion).toFixed(1);
+      vm.ledWattage = (vm.currentLumens * ledConversion).toFixed(1);
 
       // Put a limit of 24 hours in a day.
-      if ($scope.currentHours > 24) {
-        $scope.currentHours = 24;
+      if (vm.currentHours > 24) {
+        vm.currentHours = 24;
       }
 
       // Calculate the total number of hours of bulb use per year.
-      var totalHours = totalDays * $scope.currentHours;
+      var totalHours = totalDays * vm.currentHours;
 
       // Convert pence per kWh into pounds
-      var cost = $scope.currentCost / 100;
+      var cost = vm.currentCost / 100;
 
       // Calculate the cost of each bulb type.
-      $scope.incCost = ((($scope.incWattage * totalHours) / 1000) * cost).toFixed(2);
-      $scope.halCost = ((($scope.halWattage * totalHours) / 1000) * cost).toFixed(2);
-      $scope.cflCost = ((($scope.cflWattage * totalHours) / 1000) * cost).toFixed(2);
-      $scope.ledCost = ((($scope.ledWattage * totalHours) / 1000) * cost).toFixed(2);
+      vm.incCost = (((vm.incWattage * totalHours) / 1000) * cost).toFixed(2);
+      vm.halCost = (((vm.halWattage * totalHours) / 1000) * cost).toFixed(2);
+      vm.cflCost = (((vm.cflWattage * totalHours) / 1000) * cost).toFixed(2);
+      vm.ledCost = (((vm.ledWattage * totalHours) / 1000) * cost).toFixed(2);
 
       // Calculate the 10 year cost, with bulb replacement.
-      $scope.inc10yCost = (10 * $scope.incCost + incBulbCost *
-        Math.ceil(totalHours * 10 / $scope.incLife)).toFixed(2);
-      $scope.hal10yCost = (10 * $scope.halCost + halBulbCost *
-        Math.ceil(totalHours * 10 / $scope.halLife)).toFixed(2);
-      $scope.cfl10yCost = (10 * $scope.cflCost + cflBulbCost *
-        Math.ceil(totalHours * 10 / $scope.cflLife)).toFixed(2);
-      $scope.led10yCost = (10 * $scope.ledCost + ledBulbCost *
-        Math.ceil(totalHours * 10 / $scope.ledLife)).toFixed(2);
+      vm.inc10yCost = (10 * vm.incCost + incBulbCost *
+        Math.ceil(totalHours * 10 / vm.incLife)).toFixed(2);
+      vm.hal10yCost = (10 * vm.halCost + halBulbCost *
+        Math.ceil(totalHours * 10 / vm.halLife)).toFixed(2);
+      vm.cfl10yCost = (10 * vm.cflCost + cflBulbCost *
+        Math.ceil(totalHours * 10 / vm.cflLife)).toFixed(2);
+      vm.led10yCost = (10 * vm.ledCost + ledBulbCost *
+        Math.ceil(totalHours * 10 / vm.ledLife)).toFixed(2);
     };
 
     // Run the calculation the first time the page is loaded.
-    $scope.calculate();
+    vm.calculate();
 
-  }]);
+  });
 
 })();
